7c9c1a01a40ef015bf452651f4ee15fc219bfc5a
Todsaporn Sangpook <todsaporn.s@geotalent.co.th>
2020-10-16 19:21:16 +0700

    update config

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,9 +1,14 @@
 import { Config } from '@stencil/core';
+import { reactOutputTarget } from '@stencil/react-output-target';
 
 export const config: Config = {
   namespace: 'ice-todo-list',
   bundles: [{ components: ['my-component'] }, { components: ['todo-list', 'todo-item'] }],
   outputTargets: [
+    reactOutputTarget({
+      componentCorePackage: 'component-library',
+      proxiesFile: '../component-library-react/src/components.ts',
+    }),
     {
       type: 'dist',
       esmLoaderPath: '../loader',
